9f53ac7453d35db4a25c209c02e95578c267a361
Carlos Silva <cefs1984@gmail.com>
2023-03-23 16:12:06 -0300

    test: ensure SignUpController calls EmailValidator with correct email

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     '@typescript-eslint/key-spacing': 'off',
     '@typescript-eslint/strict-boolean-expressions': 'off',
     '@typescript-eslint/consistent-type-imports': 'off',
-    '@typescript-eslint/method-signature-style': 'off'
+    '@typescript-eslint/method-signature-style': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'off'
   }
 }
