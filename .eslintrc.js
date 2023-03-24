b94aaad52fa6e516fb14f4e8819bbd47c916c592
Carlos Silva <cefs1984@gmail.com>
2023-03-24 01:06:39 -0300

    refactor: transform handle to async

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     '@typescript-eslint/strict-boolean-expressions': 'off',
     '@typescript-eslint/consistent-type-imports': 'off',
     '@typescript-eslint/method-signature-style': 'off',
-    '@typescript-eslint/prefer-nullish-coalescing': 'off'
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
+    '@typescript-eslint/return-await': 'off'
   }
 }
