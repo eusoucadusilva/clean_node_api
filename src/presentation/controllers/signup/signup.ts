1b1e73d6aacc82304f8b745019cbe3befe1fcdbf
Carlos Silva <cefs1984@gmail.com>
2023-03-24 18:37:38 -0300

    chore: add jest configs and scripts

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -21,10 +21,8 @@ export class SignUpController implements Controller {
       }
       const { name, email, password, passwordConfirmation } = httpRequest.body
       if (password !== passwordConfirmation) {
-        console.log('antes do return!')
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
-      console.log('depois do return!')
       const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
@@ -36,6 +34,7 @@ export class SignUpController implements Controller {
       })
       return ok(account)
     } catch (error) {
+      console.error(error)
       return serverError()
     }
   }
