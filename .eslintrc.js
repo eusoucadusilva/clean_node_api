9e37a1e4c66d25291d8bddaefa84cf0b9a896bb3
Carlos Silva <cefs1984@gmail.com>
2023-03-26 21:08:21 -0300

    feat: ensure BcryptAdapter returns a hash on success

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     '@typescript-eslint/consistent-type-imports': 'off',
     '@typescript-eslint/method-signature-style': 'off',
     '@typescript-eslint/prefer-nullish-coalescing': 'off',
-    '@typescript-eslint/return-await': 'off'
+    '@typescript-eslint/return-await': 'off',
+    '@typescript-eslint/no-confusing-void-expression': 'off'
   }
 }
